35d17993f332aec7100e5a272de9f33e996b7580
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-12 10:24:19 -0600

    Create a domain model
    Practice creating a domain model, School. Utilize instance methods and instance variables

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    @roster.keys.include?(grade) ? @roster[grade] << name : @roster[grade] = [name]
+  end
+
+  def grade(grade)
+    @roster[grade].map {|student| student}
+  end
+
+  def sort
+    sorted_hash = @roster.each {|key, array| @roster[key] = array.sort}
+    sorted_hash
+  end
+end
